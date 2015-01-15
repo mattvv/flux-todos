b42286f7ee58390b2895bb44e4d6213b6e94d1b5
Matt Van Veenendaal <mattvanveenendaal@gmail.com>
2015-01-15 11:18:32 -0800

    Extracting stores and constants

--- app/main.js
+++ app/main.js
@@ -1,46 +1,8 @@
 var Fluxxor = require('fluxxor');
 var React = require('React');
 
-var constants = {
-  ADD_TODO: "ADD_TODO",
-  TOGGLE_TODO: "TOGGLE_TODO",
-  CLEAR_TODOS: "CLEAR_TODOS"
-};
-
-var TodoStore = Fluxxor.createStore({
-  initialize: function() {
-    this.todos = [];
-
-    this.bindActions(
-      constants.ADD_TODO, this.onAddTodo,
-      constants.TOGGLE_TODO, this.onToggleTodo,
-      constants.CLEAR_TODOS, this.onClearTodos
-    );
-  },
-
-  onAddTodo: function(payload) {
-    this.todos.push({text: payload.text, complete: false});
-    this.emit("change");
-  },
-
-  onToggleTodo: function(payload) {
-    payload.todo.complete = !payload.todo.complete;
-    this.emit("change");
-  },
-
-  onClearTodos: function() {
-    this.todos = this.todos.filter(function(todo) {
-      return !todo.complete;
-    });
-    this.emit("change");
-  },
-
-  getState: function() {
-    return {
-      todos: this.todos
-    };
-  }
-});
+var TodoStore = require('./stores/todo_store');
+var constants = require('./constants');
 
 var actions = {
   addTodo: function(text) {
