da2c1cf2ec750cc3a44aac6e73ee79f80105976d
Matt Van Veenendaal <mattvanveenendaal@gmail.com>
2015-01-15 11:16:15 -0800

    moving to app dir

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var source = require('vinyl-source-stream');
 var reactify = require('reactify');
 var concat = require('gulp-concat');
 
-gulp.task('compile', function() {
+gulp.task('default', function() {
   var b = browserify();
   b.transform(reactify); //use reactify transform
-  b.add('./main.js');
+  b.add('./app/main.js');
   return b.bundle()
-    .pipe(source('./main.js'))
+    .pipe(source('./app/main.js'))
     .pipe(gulp.dest('./dist'));
 })
